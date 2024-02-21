5f0335c630e412dda5949bb73bad6ab6a9355100
diogods7 <diogosalgueiro16@hotmail.com>
2024-02-21 17:33:50 -0300

    finalmente

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const playButton = document.getElementById('playButton');
-
-//playButton.addEventListener('click', () => {});
-
-// Adicione aqui a lógica para reiniciar o botão e parar a música quando necessário
-// Adicione este código ao seu arquivo script.js
-
+const audio = new Audio('music.mp3');
 const photos = document.querySelectorAll('.photo');
+const lyricElements = document.querySelectorAll('.lyrics h1, .lyrics h2');
+const speed = 70;
 
-// Remove a lógica de deslocamento vertical das fotos no script.js
-window.addEventListener('load', () => {
+playButton.addEventListener('click', () => {
+    playPhoto();
+    playText();
+    audio.play();
+    setTimeout(showLyrics, 100);
+});
+
+const photoTimings = [0, 2000, 4000, 6000, 8000, 10000, 12000, 14000, 16000, 18000, 20000, 22000];
+const textTimings = [0, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 55000];
+
+function playPhoto() {
+    console.log('foto');
     photos.forEach((photo, index) => {
         setTimeout(() => {
             photo.style.opacity = '1';
@@ -17,19 +24,23 @@ window.addEventListener('load', () => {
             } else {
                 photo.style.animation = 'moveAndRotate 1s ease forwards';
             }
-        }, index * 2000); // Ajuste o intervalo conforme necessário
+        }, photoTimings[index]);
     });
-});
+}
 
-var lyricElements = document.querySelectorAll('.lyrics h1, .lyrics h2');
-var currentElementIndex = 0;
-var interval;
-var speed = 70;
+function playText() {
+    lyricElements.forEach((element, index) => {
+        setTimeout(() => {
+            writeText(element);
+            console.log(textTimings[index])
+        }, textTimings[index]);
+    });
+}
 
 function writeText(element) {
-    var text = element.textContent.trim(); // Obtém o texto do elemento
+    var text = element.textContent.trim();
     var currentLetterIndex = 0;
-    var interval = setInterval(function() {
+    var interval = setInterval(function () {
         if (currentLetterIndex <= text.length) {
             var partialText = text.substring(0, currentLetterIndex);
             element.textContent = partialText;
@@ -40,21 +51,10 @@ function writeText(element) {
     }, speed);
 }
 
-window.addEventListener('load', function() {
-    lyricElements.forEach(function(element) {
-        writeText(element);
+function showLyrics() {
+    lyricElements.forEach((element, index) => {
+        setTimeout(() => {
+            element.style.opacity = '1';
+        }, textTimings[index]);
     });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
